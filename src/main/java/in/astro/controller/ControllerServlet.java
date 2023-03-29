6ae1a25050705631faa8f7c3993a1c7815d45d79
Mr-Anubhav-pandey <pandeyanubhav888@gmail.com>
2023-03-29 13:18:09 +0530

    servlet updated

--- src/main/java/in/astro/controller/ControllerServlet.java
+++ src/main/java/in/astro/controller/ControllerServlet.java
@@ -49,6 +49,74 @@ public class ControllerServlet extends HttpServlet {
 
 			}
 		}
+		if(request.getPathInfo().equals("/searchform")) {
+			String sid = request.getParameter("sid");
+			Student student = studentService.searchStudent(Integer.parseInt(sid));
+			PrintWriter out = response.getWriter();
+			if(student!=null) {
+				out.println("<body>");
+				out.println("<center>");
+				out.println("<table border='1px solid black'>");
+				out.println("<tr><th>Name</th><th>age</th><th>Address</th></tr>");
+				out.println("<tr><td>"+student.getSname()+"</td><td>"+student.getSage()+"</td><td>"+student.getSaddress()+"</td></tr>");
+				out.println("</table>");
+				out.println("</center>");
+				out.println("</body>");
+			}else {
+				out.println("<center>");
+				out.println("<h2 style='color:green;'>Not Found!! </h1>");
+				out.println("</center>");
+			}
+			out.close();
+			
+		}
+		if(request.getPathInfo().equals("/deleteform")) {
+			String sid = request.getParameter("sid");
+			String status = studentService.deleteStudent(Integer.parseInt(sid));
+			PrintWriter out = response.getWriter();
+			if(status.equals("success")) out.println("<h1 style='color:green; text-align:center;'>Deleted Successfully</h1>");
+			else out.println("<h1 style='color:red; text-align:center;'>Record Not Found!!</h1>");
+		}
+		if(request.getPathInfo().equals("/editform")) {
+			String sid = request.getParameter("sid");
+			Student searchStudent = studentService.searchStudent(Integer.parseInt(sid));
+			PrintWriter out = response.getWriter();
+			if(searchStudent!=null) {
+				out.println("<body>");
+				out.println("<center>");
+				out.println("<form method='post' action='./updateRecord'>");
+				out.println("<table>"); 
+				out.println("<tr><th>ID</th><td>"+searchStudent.getSid()+"</td></tr>");
+				System.out.println(searchStudent.getSid());
+				out.println("<input type='hidden' name='sid' value='"+searchStudent.getSid()+"'/>");
+				out.println("<tr><th>NAME</th><td><input type='text' name='sname' value='"+searchStudent.getSname()+"'/></td></tr>");
+				out.println("<tr><th>AGE</th><td><input type='text' name='sage' value='"+searchStudent.getSage()+"'/></td></tr>");
+				out.println("<tr><th>ADDRESS</th><td><input type='text' name='saddr' value='"+searchStudent.getSaddress()+"'/></td></tr>");
+				out.println("<tr><td></td><td><input type='submit' value='Search' /></td></tr>");
+				out.println("</table>");
+				out.println("</form>");
+				out.println("</center>"); 
+				out.println("</body>");
+			}else {
+				out.println("<h1 style='color:red; text-align:center;'>Record Not Found!!</h1>");
+			}
+			out.close();
+		}
+		if(request.getPathInfo().equals("/updateRecord")) {
+			PrintWriter out = response.getWriter();
+			String sid = request.getParameter("sid");
+			String sname = request.getParameter("sname");
+			String sage = request.getParameter("sage");
+			String saddress = request.getParameter("saddr");
+			Student updateStudent = new Student();
+			updateStudent.setSaddress(saddress);
+			updateStudent.setSid(Integer.parseInt(sid));
+			updateStudent.setSname(sname);
+			updateStudent.setSage(Integer.parseInt(sage));
+			String status = studentService.updateStudent(updateStudent);
+			if(status.equals("success")) out.println("<h1 style='color:green; text-align:center;'>Update Successfully</h1>");
+			else out.println("<h1 style='color:red; text-align:center;'>This Shouldn't be happening!!</h1>");
+		}
 	}
 
 }
