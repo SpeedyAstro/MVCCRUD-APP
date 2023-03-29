d485cc34a8158c4ad89d35698de34eb4756f5cc2
Mr-Anubhav-pandey <pandeyanubhav888@gmail.com>
2023-03-29 13:18:41 +0530

    `DBCRUD`

--- src/main/java/in/astro/dao/StudentDaoImpl.java
+++ src/main/java/in/astro/dao/StudentDaoImpl.java
@@ -38,20 +38,75 @@ public class StudentDaoImpl implements IStudentDao {
 
 	@Override
 	public Student searchStudent(Integer sid) {
-		// TODO Auto-generated method stub
-		return null;
+		connection = null;
+		statement = null;
+		Student student = new Student();
+		String query = "select name,age,address from students where id=?";
+		try {
+			connection = JdbcUtil.getConnection();
+			if(connection!=null) {
+				statement = connection.prepareStatement(query);
+				if(statement!=null) {
+					statement.setInt(1, sid);
+					resultset = statement.executeQuery();
+					if(resultset!=null) {
+						if(resultset.next()) {
+							student.setSid(sid);
+							student.setSname(resultset.getString(1));
+							student.setSage(resultset.getInt(2));
+							student.setSaddress(resultset.getString(3));
+							System.out.print(student);
+							return student;
+						}
+					}
+				}
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return student;
 	}
 
 	@Override
 	public String deleteStudent(Integer sid) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "delete from students where id=?";
+		try {
+			connection = JdbcUtil.getConnection();
+			if(connection!=null) {
+				statement = connection.prepareStatement(query);
+				if(statement!=null) {
+					statement.setInt(1, sid);
+					int rowaffected = statement.executeUpdate();
+					if(rowaffected == 1) return "success";
+				}
+			}
+		}catch(SQLException | IOException e) {
+			e.printStackTrace();
+		}
+		return "failure";
 	}
 
 	@Override
 	public String updateStudent(Student student) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "update students set name=?,age=?,address=? where id=?";
+		try {
+			connection = JdbcUtil.getConnection();
+			if(connection!=null) {
+				statement = connection.prepareStatement(query);
+				if(statement!=null) {
+					statement.setString(1, student.getSname());
+					statement.setInt(2, student.getSage());
+					statement.setString(3, student.getSaddress());
+					statement.setInt(4, student.getSid());
+					String status = "failure";
+					int rowaffected = statement.executeUpdate();
+					if(rowaffected==1)	return "success";
+				}
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return "failure";
 	}
 
 }
